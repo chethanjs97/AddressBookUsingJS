49c5277c376fb6e0a75e5a82322a61d75ca400fa
chethanjs97 <chethanjschethu@gmail.com>
2020-12-07 20:38:31 +0530

    Added: Validating User Entries With Regex

--- AddressBook.js
+++ AddressBook.js
@@ -11,21 +11,65 @@ class AddressBook{
     }
 
     get firstName() {return this._firstName;}
-    set firstName(firstName)    {this._firstName=firstName;}
+    set firstName(firstName) {
+        var nameRegex=RegExp('^[A-Z][a-z]{2,}$');
+        if(nameRegex.test(firstName))
+            this._firstName=firstName;
+        else
+            throw 'First name is incorrect.';
+        }
     get lastname() {return this._lastname;}
-    set lastname(lastname)    {this._lastname=lastname;}
+    set lastname(lastname) {
+        if(nameRegex.test(lastname))
+            this._lastname=lastname;
+        else
+            throw 'Last name is incorrect.';
+        }
     get address() {return this._address;}
-    set address(address)    {this._address=address;}
+    set address(address) {
+        var addrRegex=RegExp('^[A-Z][a-z]{3,}$');
+        if(addrRegex.test(address))
+            this._address=address;
+        else
+            throw 'Address is incorrect.';
+        }
     get city() {return this._city;}
-    set city(city)    {this._city=city;}
+    set city(city) {
+        if(addrRegex.test(city))
+            this._city=city;
+        else
+            throw 'City is incorrect.';
+    }
     get state() {return this._state;}
-    set state(state)    {this._state=state;}
+    set state(state) {
+        if(addrRegex.test(state))
+            this._state=state;
+        else
+            throw 'State is incorrect.';}
     get zip() {return this._zip;}
-    set zip(zip)    {this._zip=zip;}
+    set zip(zip) {
+        let zipCodeRegex=RegExp('^[1-9]{1}[0-9]{2}[ ]?[0-9]{3}$');
+        if(zipCodeRegex.test(zip))
+            this._zip=zip;
+        else
+            throw 'Zip code is incorrect.';
+        }
     get phone() {return this._phone;}
-    set phone(phone)    {this._phone=phone;}
+    set phone(phone) {
+        let phoneRegex=RegExp('^[1-9]{1}[0-9]{2}[ ]?[0-9]{3}$');
+        if(phoneRegex.test(phone))
+            this._phone=phone;
+        else
+            throw 'Phone number is incorrect.';
+        }
     get email() {return this._email;}
-    set email(email)    {this._email=email;}
+    set email(email)    {
+        let emailRegex=RegExp('^[a-zA-Z0-9]{1,}([_+-.][a-zA-Z0-9]+)*@[a-zA-Z0-9]{2,}.[a-z]{2,4}([.][a-zA-Z]{2})*$');
+        if(emailRegex.test(email))
+            this._email=email;
+        else
+            throw 'Email is incorrect.';
+    }
 
     toString(){
         return "First Name: "+this.firstName+", Last Name: "+this.lastname+", Address: "+
